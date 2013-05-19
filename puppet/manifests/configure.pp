df1a15f569fad405de8a3089df0f09fddcf16a47
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2013-05-19 16:16:53 -0400

    Removed the node definition because it's unnecessary, added a different comment to the index file so I can make sure I know it changed

--- puppet/manifests/configure.pp
+++ puppet/manifests/configure.pp
@@ -1,33 +1,26 @@
-# include baseline
+include httpd
+include build
+include mysqld
 
-node default { 
-    
-    include httpd
-    include build
-    include mysqld
-    #include xdebug
+file { '/var/www/html/index.php' :
+    ensure => present,
+    content => "<?php echo 'Internet unlocked: ' . date('Y m d : g m s');",
+    require => Package['httpd']
+}
 
-    file { '/var/www/html/index.html' :
-        ensure => present,
-        content => "<!-- welcome to the internet -->",
-        require => Package['httpd']
-    }
+file { '/var/www/html/info.php' :
+    ensure => present,
+    content => "<?php phpinfo();",
+    require => Package['httpd']
+}
 
-    file { '/var/www/html/info.php' :
-        ensure => present,
-        content => "<?php phpinfo();",
-        require => Package['httpd']
-    }
+file { '/var/www/html/.htaccess' :
+    ensure => present,
+    content => "SetEnv APPLICATION_ENV \"development\"",
+    require => Package['httpd']
+}
 
-    file { '/var/www/html/.htaccess' :
-        ensure => present,
-        content => "SetEnv APPLICATION_ENV \"development\"",
-        require => Package['httpd']
-    }
-
-    exec { "set timezone":
-      command => "/bin/ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime",
-      refreshonly => true
-    }
-    
+exec { "set timezone":
+  command => "/bin/ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime",
+  refreshonly => true
 }
